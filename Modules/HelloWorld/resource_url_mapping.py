2ef2789bb9941f8e63438002bceff02d7e46c34b
nsnkiller <172902083@qq.com>
2018-07-04 15:20:59 +0800

    add extra class in helloworld module

--- Modules/HelloWorld/resource_url_mapping.py
+++ Modules/HelloWorld/resource_url_mapping.py
@@ -1,3 +1,5 @@
 resource_url_mapping = {
-    "Modules.HelloWorld.helloworld.TodoSimple" : "/<string:todo_id>"
+    "Modules.HelloWorld.helloworld.TodoSimple" : "/<string:todo_id>",
+    "Modules.HelloWorld.helloworld.TodoList" : "/todos",
+    "Modules.HelloWorld.helloworld.Todo" : "/todos/<todo_id>"
 }
